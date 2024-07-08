6e121540e3ba2943418e547b50763bce0056e4f1
James Walker <james@aberrantrealities.com>
2024-07-08 22:04:52 +1000

    fix tag name

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,7 +25,7 @@ public class Player : Actor
 	{
 		inventory = GetComponent<PlayerInventory>();
 		health = GetComponent<RegeneratingHealth>();
-		spawnPosition = GameObject.FindWithTag("PlayerSpawn").transform;
+		spawnPosition = GameObject.FindWithTag("SpawnPoint").transform;
 		RespawnPlayer();
 	}
 
