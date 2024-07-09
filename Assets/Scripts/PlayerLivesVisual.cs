aef3a1ded5127a499a171e5d7d0bb7d5c5e30972
James Walker <james@aberrantrealities.com>
2024-07-09 20:20:43 +1000

    lives working

---
+++ Assets/Scripts/PlayerLivesVisual.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class PlayerLivesVisual : MonoBehaviour
+	{
+		public List<GameObject> lifeVisuals = new List<GameObject>();
+		public GameObject lifeAddedParticle;
+		public GameObject lifeRemovedParticle;
+		public int previousLives = 3;
+
+		public void OnLifeStateChanged(int lives)
+		{
+			if (lives > previousLives)
+			{
+				var index = lives - 1;
+				lifeVisuals[index].SetActive(true);
+				Instantiate(lifeAddedParticle, lifeVisuals[index].transform.position, lifeVisuals[index].transform.rotation);
+			}
+			if (lives < previousLives)
+			{
+				lifeVisuals[lives].SetActive(false);
+				Instantiate(lifeRemovedParticle, lifeVisuals[lives].transform.position, lifeVisuals[lives].transform.rotation);
+
+			}
+			previousLives = lives;
+		}
+	}
+}
