e3304932ad7faa5ad66e00116289624f06bfeb62
James Walker <james@aberrantrealities.com>
2024-07-11 19:40:40 +1000

    tidy

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,12 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
